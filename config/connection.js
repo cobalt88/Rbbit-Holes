809103914a6c481b11c9e17f1cc596ccffe674b1
cobalt88 <vincentteune@gmail.com>
2022-12-22 19:12:08 -0500

    merge resolution

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,10 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-<<<<<<< HEAD
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-=======
 // create connection to our db
 const sequelize = (process.env.JAWSDB_URL)
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
->>>>>>> 387570ad83e4e94864a348dbcc60a1b1df3c92d0
       host: 'localhost',
       dialect: 'mysql',
       port: 3306,
